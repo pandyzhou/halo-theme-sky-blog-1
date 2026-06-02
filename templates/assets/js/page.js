2e46cd7e3457024fc6620ec609119307d2ad31df
sky <82988354+sky121666@users.noreply.github.com>
2026-06-02 08:31:06 +0800

    chore: release 2.2.23

--- templates/assets/js/page.js
+++ templates/assets/js/page.js
@@ -1,1 +1,1 @@
-import{r as n,t}from"./chunks/page-runtime-2.2.22.js";import"./chunks/article-content-2.2.22.js";n(()=>{i()});function i(){console.log("Page content initialized")}t();
+import{r as n,t}from"./chunks/page-runtime-2.2.23.js";import"./chunks/article-content-2.2.23.js";n(()=>{i()});function i(){console.log("Page content initialized")}t();
